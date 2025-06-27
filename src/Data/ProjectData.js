76032580777cdd1d07d7c30c8b8032fcd05db572
이재성 <jaeseong@ijaeseong-ui-MacBookAir.local>
2025-06-27 15:15:15 +0900

    이미지사이즈변경

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -3,10 +3,10 @@ const projectData = [
     title: "Carhartt",
     koTitle: "칼하트",
     image: {
-      PC: "/portfolio/portfolioimage/carhartt/carhartt1920.png",
-      laptop: "/portfolio/portfolioimage/carhartt/carhartt1024.png",
-      tablet: "/portfolio/portfolioimage/carhartt/carhartt768.png",
-      mobile: "/portfolio/portfolioimage/carhartt/carhartt360.png",
+      PC: "/portfolio/portfolioimage/carhartt/carhartt1920.webp",
+      laptop: "/portfolio/portfolioimage/carhartt/carhartt1024.webp",
+      tablet: "/portfolio/portfolioimage/carhartt/carhartt768.webp",
+      mobile: "/portfolio/portfolioimage/carhartt/carhartt360.webp",
     },
     description: "선호하는 의류브랜드 사이트를 리뉴얼해서 만들었습니다",
     details: {
@@ -25,10 +25,10 @@ const projectData = [
     title: "Tesla",
     koTitle: "테슬라",
     image: {
-      PC: "/portfolio/portfolioimage/tesla/teslaPC.png",
-      laptop: "/portfolio/portfolioimage/tesla/teslaLaptop.png",
-      tablet: "/portfolio/portfolioimage/tesla/teslaTablet.png",
-      mobile: "/portfolio/portfolioimage/tesla/teslaMobile.png",
+      PC: "/portfolio/portfolioimage/tesla/teslaPC.webp",
+      laptop: "/portfolio/portfolioimage/tesla/teslaLaptop.webp",
+      tablet: "/portfolio/portfolioimage/tesla/teslaTablet.webp",
+      mobile: "/portfolio/portfolioimage/tesla/teslaMobile.webp",
     },
     description: "선호하는 차 브랜드 사이트를 리뉴얼해서 만들었습니다",
     details: {
@@ -47,10 +47,10 @@ const projectData = [
     title: "TodoList",
     koTitle: "투두리스트",
     image: {
-      PC: "/portfolio/portfolioimage/TodoList/TodoListdesktop.png",
-      laptop: "/portfolio/portfolioimage/TodoList/TodoListlaptop.png",
-      tablet: "/portfolio/portfolioimage/TodoList/TodoListtablet.png",
-      mobile: "/portfolio/portfolioimage/TodoList/TodoListmobile.png",
+      PC: "/portfolio/portfolioimage/TodoList/TodoListdesktop.webp",
+      laptop: "/portfolio/portfolioimage/TodoList/TodoListlaptop.webp",
+      tablet: "/portfolio/portfolioimage/TodoList/TodoListtablet.webp",
+      mobile: "/portfolio/portfolioimage/TodoList/TodoListmobile.webp",
     },
     description: "리액트를 공부하며 만든 투두리스트 앱 입니다",
     details: {
@@ -69,10 +69,10 @@ const projectData = [
     title: "FlimFind",
     koTitle: "필름파인드",
     image: {
-      PC: "/portfolio/portfolioimage/FilmFind/FilmFIndDesktop.png",
-      laptop: "/portfolio/portfolioimage/FilmFind/FilmFIndlaptop.png",
-      tablet: "/portfolio/portfolioimage/FilmFind/FilmFIndtablet.png",
-      mobile: "/portfolio/portfolioimage/FilmFind/FilmFIndMobile.png",
+      PC: "/portfolio/portfolioimage/FilmFind/FilmFIndDesktop.webp",
+      laptop: "/portfolio/portfolioimage/FilmFind/FilmFIndlaptop.webp",
+      tablet: "/portfolio/portfolioimage/FilmFind/FilmFIndtablet.webp",
+      mobile: "/portfolio/portfolioimage/FilmFind/FilmFIndMobile.webp",
     },
     description: "리액트를 공부하며 api를 받아와 만든 영화소개 사이트입니다",
     details: {
